1e5b7781b80000a56fd4d57438087ef9f1b1a152
katedayun <58518683+katedayun@users.noreply.github.com>
2023-11-05 21:44:03 +0900

    03-2

--- main1.c
+++ main1.c
@@ -7,19 +7,20 @@
 int main(int argc, char *argv[1])
 {
     int i;
-    int grade[N_STUDENT];
-    int score[N_STUDENT];
+    int a[N_STUDENT] = {1, 2, 3, 4, 5};
+    int b[N_STUDENT] = {1, 2, 3, 4, 5};
+    int flag = 0;
 
-    for(i=0;i<N_STUDENT;i++)
-        score[i] = grade[i];
-
-    for(i=0;i<N_STUDENT;i++)
-        printf("score[%d] = %d (grade:%i)\n", i, score[i], grade[i]);
-
+    for (i=0;i<N_STUDENT;i++)
+    {
+        if (a[i] != b[i])
+        {
+            printf("array a and b are not same\n");
+            flag=1;
+        }
+    }
+    if (flag == 0)
+            printf("array a and b are same\n");
     return 0;
  }
- //score[0] = 0 (grade:0)
-//score[1] = 0 (grade:0)
-//score[2] = 48 (grade:48)
-//score[3] = 0 (grade:0)
-//score[4] = 0 (grade:0)
+ //array a and b are same
