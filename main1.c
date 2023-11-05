aebd60a0838add58f010cd3dd3dc4548e8efdc27
katedayun <58518683+katedayun@users.noreply.github.com>
2023-11-05 22:00:56 +0900

    05

--- main1.c
+++ main1.c
@@ -2,34 +2,47 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+#include <stdio.h>
+#define ROWS 3
+#define COLS 3
 
-#define SIZE   4
-
-void square_array(int a[], int size)
-{
-    int i;
-
-    for (i=0;i<size;i++)
-        a[i] = a[i]*a[i];
+void addMatrix(int A[][COLS], int B[][COLS], int C[][COLS]) {
+    int i, j; // Declare loop counters outside the 'for' loop
+    for (i = 0; i < ROWS; i++) {
+        for (j = 0; j < COLS; j++) {
+            C[i][j] = A[i][j] + B[i][j];
+        }
+    }
 }
 
-void print_array(int a[], int size)
-{
-    int i;
-
-    for (i=0;i<size;i++)
-        printf("%3d", a[i]);
-    printf("\n");
+void printMatrix(int A[][COLS]) {
+    int i, j; // Declare loop counters outside the 'for' loop
+    for (i = 0; i < ROWS; i++) {
+        for (j = 0; j < COLS; j++) {
+            printf("%d ", A[i][j]);
+        }
+        printf("\n");
+    }
 }
 
-int main(int argc, char *argv[1])
-{
-    int list[SIZE] = {1, 2, 3, 4};
+int main() {
+    int A[ROWS][COLS] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+    int B[ROWS][COLS] = {
+        {9, 8, 7},
+        {6, 5, 4},
+        {3, 2, 1}
+    };
+    int C[ROWS][COLS]; // This will hold the result of A + B
 
-    print_array(list, SIZE);
-    square_array(list, SIZE);
-    print_array(list, SIZE);
+    addMatrix(A, B, C);
+
+    printf("Matrix C (Result of A + B):\n");
+    printMatrix(C);
+
     return 0;
- }
- //  1  2  3  4
-//   1  4  9 16
+}
+
